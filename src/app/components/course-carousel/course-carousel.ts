63a1a3ab37d69e4fa760af90817d3c97f367f2dc
Lorenzzoczn <lorenzzo321@gmail.com>
2025-10-09 20:04:35 -0300

    Adiciona pasta fulldev-angular

---
+++ src/app/components/course-carousel/course-carousel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-carousel',
+  imports: [],
+  templateUrl: './course-carousel.html',
+  styleUrl: './course-carousel.css'
+})
+export class CourseCarousel {
+
+}
---
+++ src/app/components/posts-section/posts-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-section',
+  imports: [],
+  templateUrl: './posts-section.html',
+  styleUrl: './posts-section.css'
+})
+export class PostsSection {
+
+}
